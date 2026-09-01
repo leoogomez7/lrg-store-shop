09803a4fd1c054e3e9cc2b2f61a59e7919b1b070
leoogomez7 <campeonleonardo@hotmail.com>
2026-09-01 17:53:17 -0300

    error de runtime en pedidos solucionado

--- src/routes/admin.productos.tsx
+++ src/routes/admin.productos.tsx
@@ -45,6 +45,7 @@ import {
   DialogFooter,
   DialogHeader,
   DialogTitle,
+  DialogTrigger,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
